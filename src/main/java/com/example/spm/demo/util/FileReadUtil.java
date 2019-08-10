8ed3e5a6cda168beed8302a771862440a277871e
InfiniteCoder96 <pasinduvimansa96@gmail.com>
2019-08-10 15:33:12 +0530

    Ancestor classes detection done

--- src/main/java/com/example/spm/demo/util/FileReadUtil.java
+++ src/main/java/com/example/spm/demo/util/FileReadUtil.java
@@ -1,17 +1,10 @@
 package com.example.spm.demo.util;
 
-import org.springframework.core.io.FileSystemResource;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.ResourceLoader;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ResponseBody;
-
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.ListIterator;
+import java.util.stream.Collectors;
 
 public class FileReadUtil {
 
@@ -20,51 +13,108 @@ public class FileReadUtil {
 
         File file = new File(filePath);
 
-        int numberOfLines = 0;
 
-        List<String> wordArrayList = null;
-        List<String> ancestorClasses = new ArrayList<>();
-        int ancestorClassCount = 0;
-        int count = 0;
+
         try {
             FileReader fileReader = new FileReader(file);
             BufferedReader reader = new BufferedReader(fileReader);
-            String line;
 
             String filePaths = file.getPath();
             String fileExtension = filePath.substring(filePath.indexOf('.') + 1);
             System.out.println(fileExtension);
             System.out.println(filePaths);
-            while (true) {
-                line = reader.readLine();
-                if (line == null)
-                    break;
-                wordArrayList = Arrays.asList(line.split("\\W+"));
 
-                wordArrayList.replaceAll(String::trim);
+            switch (fileExtension){
+                case "java":
+                    return this.processJavaCode(reader);
 
-                for (String word : wordArrayList){
-                    if(word.equals("extends")) {
+                case "cp":
+                    return this.processCplusCode(reader);
 
-                        int c = wordArrayList.indexOf("extends");
+                default:
 
-                        ancestorClasses.add(wordArrayList.get(c + 1));
+            }
 
-                        ancestorClassCount++;
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        return null;
+    }
+
+    private List<String> processJavaCode(BufferedReader reader) throws IOException {
+
+        String line;
+        List<String> wordArrayList;
+        List<String> ancestorClasses = new ArrayList<>();;
+        int numberOfLines = 0;
+        int ancestorClassCount = 0;
+
+
+        while (true) {
+            line = reader.readLine();
+            if (line == null)
+                break;
+            wordArrayList = Arrays.asList(line.split("\\W+"));
+
+            wordArrayList.replaceAll(String::trim);
+
+            for (String word : wordArrayList){
+                if(word.equals("extends")) {
+
+                    int c = wordArrayList.indexOf("extends");
+
+                    ancestorClasses.add(wordArrayList.get(c + 1));
+
+                    ancestorClassCount++;
+                }
+            }
+
+
+            numberOfLines++;
+        }
+
+        return ancestorClasses;
+    }
+
+    private List<String> processCplusCode(BufferedReader reader) throws IOException {
+
+        String line;
+        List<String> wordArrayList;
+        List<String> listWithDuplicates = new ArrayList<>();
+
+        int numberOfLines = 0;
+        int ancestorClassCount = 0;
+
+        while (true) {
+            line = reader.readLine();
+            if (line == null)
+                break;
+
+            if(line.contains(":")) {
+                String x = line.substring(line.indexOf(":"));
+
+                wordArrayList = Arrays.asList(x.split("\\W+"));
+
+
+                for(int i = 0; i < wordArrayList.size(); i++){
+                    if(wordArrayList.get(i).trim().equals("public")){
+
+                        listWithDuplicates.add(wordArrayList.get(i + 1));
+
                     }
                 }
 
-                System.out.println(wordArrayList);
-                numberOfLines++;
             }
-            reader.close();
 
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+            numberOfLines++;
+
         }
-        System.out.println(wordArrayList);
-        return ancestorClasses;
 
+
+        return listWithDuplicates.stream().distinct().collect(Collectors.toList());
     }
 }
